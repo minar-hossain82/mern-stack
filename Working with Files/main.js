f7c5d05188107487ceba97a18db49aec9d00fbe8
minar-hossain82 <minarhossain986@gmail.com>
2025-12-04 20:10:18 +0600

    Update Working with Files/main.js

--- Working with Files/main.js
+++ Working with Files/main.js
@@ -1,18 +1,21 @@
-const fs = require("fs")
-// const fs = require("fs/promises")
- 
-console.log("starting")
-// fs.writeFileSync("harry.txt", "Harry is a good boy")
+const fs = require("fs")   // Import the built-in filesystem module
+// const fs = require("fs/promises") // Alternative: promise-based version
 
-fs.writeFile("harry2.txt", "Harry is a good boy2", ()=>{
-    console.log("done")
-    fs.readFile("harry2.txt", (error, data)=>{
-        console.log(error, data.toString())
+console.log("starting")    // Log the start of the program
+
+// Write to a file asynchronously (non-blocking)
+fs.writeFile("harry2.txt", "Harry is a good boy2", () => {
+    console.log("done")    // Runs after writing is complete
+
+    // Read the file asynchronously
+    fs.readFile("harry2.txt", (error, data) => {
+        console.log(error, data.toString()) // Log any error and file content
     })
 })
 
-fs.appendFile("harry.txt", "harryrobo", (e, d)=>{
-    console.log(d)
+// Append text to another file asynchronously
+fs.appendFile("harry.txt", "harryrobo", (e, d) => {
+    console.log(d)         // d is undefined for appendFile, but callback still runs
 })
 
-console.log("ending")
+console.log("ending")      // Logs before async operations finish (non-blocking behavior)
