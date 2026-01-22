426f9dbc81bc43af486f0f9d8aa207a77be0a5fb
minar-hossain82 <minarhossain986@gmail.com>
2026-01-22 17:30:14 +0600

    Update Advanced Javascript/script.js

--- Advanced Javascript/script.js
+++ Advanced Javascript/script.js
@@ -1,42 +1,30 @@
-console.log(a1); // Logs the value of a1 (hoisted but undefined here)
+console.log(a1);
 
 (async function main() {
-  // Main async IIFE (Immediately Invoked Function Expression)
-
-  // let a = await sleep() // Example of waiting for sleep()
-  // console.log(a)
-  // let b = await sleep() // Another await example
-  // console.log(b);
-
-  // let [x, y, rest] = [1, 5, 7, 8, 9, 10] // Array destructuring example
-  // console.log(x, y, rest)
-
   let obj = {
     a: 1,
     b: 2,
     c: 3,
-  }; // Object with three key-value pairs
+  };
 
-  let { a, b } = obj; // Object destructuring: extract 'a' and 'b'
-  console.log(a, b); // Logs 1 and 2
+  let { a, b } = obj;
+  console.log(a, b);
 
-  let arr = [1, 4, 6]; // Array of three numbers
-  console.log(sum(arr[0], arr[1], arr[2])); // Passing elements manually
-  console.log(sum(...arr)); // Spread syntax for cleaner passing
+  let arr = [1, 4, 6];
+  console.log(await sum(arr[0], arr[1], arr[2]));
+  console.log(await sum(...arr));
 })();
 
-var a1 = 6; // Variable declared (hoisted but assigned here)
+var a1 = 6;
 
 const sleep = async () => {
-  // Returns a promise that resolves after 1 second
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(45); // Promise resolved with value 45
+      resolve(45);
     }, 1000);
   });
 };
 
 const sum = async (a, b, c) => {
-  // Async function returning sum of three values
   return a + b + c;
 };
