1dabd1851dd8e002436657ca109a4f0fc9657279
minar-hossain82 <minarhossain986@gmail.com>
2026-01-24 22:17:25 +0600

    Update Classes & Objects/gs.js

--- Classes & Objects/gs.js
+++ Classes & Objects/gs.js
@@ -1,29 +1,23 @@
 class User {
   constructor(name) {
-    // Calls the setter method to initialize the name property
     this.name = name;
   }
 
   get name() {
-    // Returns the internal _name value
     return this._name;
   }
 
   set name(value) {
-    // Validates that the name has at least 4 characters
     if (value.length < 4) {
       console.log("Name is too short.");
-      return; // Stops further execution without setting a new name
+      return;
     }
-    // Stores the valid name in a protected variable _name
     this._name = value;
   }
 }
 
-// Creates a new User object with a valid name
 let user = new User("John");
-console.log(user.name); // John
+console.log(user.name);
 
-// Attempts to set a name that is too short, so setter rejects it
-user.name = "Harry"; // Name is too short.
-console.log(user.name); // John (previous value remains)
+user.name = "Harry";
+console.log(user.name);
