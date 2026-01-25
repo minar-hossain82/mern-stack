a7fca5de4b301fe60e768eee1cfd09e83a762d65
minar-hossain82 <minarhossain986@gmail.com>
2026-01-25 16:44:50 +0600

    Update CommonJs Vs EcmaScript Modules/main.js

--- CommonJs Vs EcmaScript Modules/main.js
+++ CommonJs Vs EcmaScript Modules/main.js
@@ -1,16 +1,3 @@
-// import {a, b, d} from "./mymodule.js"
-// console.log(a, b, d)
+const a = require("./mymodule2.js");
 
-
-// import harry from "./mymodule.js"
-// console.log(harry)
-
-// (function(exports, require, module, __filename, __dirname) {
-
-//     // Module code actually lives here
-  
-//   });
-
-const a = require("./mymodule2.js")
-
-console.log(a, __dirname, __filename)
+console.log(a, __dirname, __filename);
