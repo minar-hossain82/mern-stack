f3bfc181f74fdb64d4f05684cd2a5af2fc9f33a1
minar-hossain82 <minarhossain986@gmail.com>
2026-01-25 16:39:13 +0600

    Update Classes & Objects/script.js

--- Classes & Objects/script.js
+++ Classes & Objects/script.js
@@ -1,49 +1,32 @@
-// let obj = {
-//     a: 1,
-//     b: "Harry"
-// }
+class Animal {
+  constructor(name) {
+    this.name = name;
+    console.log("Object is created...");
+  }
 
-// console.log(obj)
+  eats() {
+    console.log("Kha raha hoon");
+  }
 
-// let animal = {
-//     eats: true
-// };
-
-// let rabbit = {
-//     jumps: true
-// };
-
-// rabbit.__proto__ = animal; // sets rabbit.[[Prototype]] = animal
-
-class Animal{
-    constructor(name){
-        this.name = name
-        console.log("Object is created...")
-    }
-
-    eats(){
-        console.log("Kha raha hoon")
-    }
-    jumps(){
-        console.log("Kood raha hoon")
-    }
+  jumps() {
+    console.log("Kood raha hoon");
+  }
 }
 
-
 class Lion extends Animal {
-    constructor(name){
-        super(name) 
-        console.log("Object is created and he is a lion...")
-    }
+  constructor(name) {
+    super(name);
+    console.log("Object is created and he is a lion...");
+  }
 
-    eats(){
-        super.eats()
-        console.log("Kha raha hoon roar")
-    }
+  eats() {
+    super.eats();
+    console.log("Kha raha hoon roar");
+  }
 }
 
 let a = new Animal("Bunny");
-console.log(a)
+console.log(a);
 
-let l = new Lion("Shera")
-console.log(l)
+let l = new Lion("Shera");
+console.log(l);
