a7d66f6a0991baa62e9eed3d0b293ff64b3bddaa
minar-hossain82 <minarhossain986@gmail.com>
2026-02-09 13:36:05 +0600

    Added Exercise 11 - Solution/Exercise 13-Dynamic Website Builder/

---
+++ Exercise 11 - Solution/Exercise 13-Dynamic Website Builder/script.js
@@ -0,0 +1,24 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+  // title: title of the video
+  // cName: channel name
+  // views: total number of views on the video
+  // monthsOld: how many months ago the video was uploaded
+  // duration: length of the video (e.g., "31:22")
+  // thumbnail: URL of the video thumbnail image
+  // This function should create a video card dynamically
+  // 1. Create the main card container element
+  // 2. Add the thumbnail image to the card
+  // 3. Display the video title
+  // 4. Show channel name, views, and upload time
+  // 5. Show the video duration overlay on the thumbnail
+  // 6. Append the card to the main container in the HTML
+}
+
+createCard(
+  "Introduction to Backend | Sigma Web Dev video #2",
+  "CodeWithHarry",
+  560000,
+  7,
+  "31:22",
+  "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindK92kYMgTcQbw",
+);
