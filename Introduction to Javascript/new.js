7190ec7185ebb3cf3c833a230ad0d147a6973e80
minar-hossain82 <minarhossain986@gmail.com>
2026-02-16 08:39:16 +0600

    Update Introduction to Javascript/new.js

--- Introduction to Javascript/new.js
+++ Introduction to Javascript/new.js
@@ -1,11 +1,4 @@
-// Prints "Hello World" to the console
 console.log("Hello World");
-
-// Indicates that the code execution has started
 console.log("Code is running...");
-
-// Confirms that the code continues to run
 console.log("Code is also running...");
-
-// Displays a playful message about the code output
 console.log("Code is looking like a wow...");
