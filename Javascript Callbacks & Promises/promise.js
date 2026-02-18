0da286dde831d0db5ad0a5678a95258ce44958f6
minar-hossain82 <minarhossain986@gmail.com>
2026-02-18 09:43:48 +0600

    Update Javascript Callbacks & Promises/promise.js

--- Javascript Callbacks & Promises/promise.js
+++ Javascript Callbacks & Promises/promise.js
@@ -1,16 +1,11 @@
-// Log a message to indicate the start of Promises example
 console.log("This is Promises");
 
-// Create the first promise
 let prom1 = new Promise((resolve, reject) => {
-  // Generate a random number between 0 and 1
   let a = Math.random();
 
-  // If the random number is less than 0.5, reject the promise
   if (a < 0.5) {
     reject("No random number was not supporting you");
   } else {
-    // Otherwise, resolve the promise after 3 seconds
     setTimeout(() => {
       console.log("Yes I am done");
       resolve("Harry");
@@ -18,16 +13,12 @@ let prom1 = new Promise((resolve, reject) => {
   }
 });
 
-// Create the second promise
 let prom2 = new Promise((resolve, reject) => {
-  // Generate a random number between 0 and 1
   let a = Math.random();
 
-  // If the random number is less than 0.5, reject the promise
   if (a < 0.5) {
     reject("No random number was not supporting you 2");
   } else {
-    // Otherwise, resolve the promise after 1 second
     setTimeout(() => {
       console.log("Yes I am done 2");
       resolve("Harry 2");
@@ -35,16 +26,10 @@ let prom2 = new Promise((resolve, reject) => {
   }
 });
 
-// Promise.race returns a promise that settles
-// as soon as the first promise resolves or rejects
 let p3 = Promise.race([prom1, prom2]);
 
-// If the first settled promise is resolved, this runs
 p3.then((a) => {
   console.log(a);
-})
-
-  // If the first settled promise is rejected, this runs
-  .catch((err) => {
-    console.log(err);
-  });
+}).catch((err) => {
+  console.log(err);
+});
