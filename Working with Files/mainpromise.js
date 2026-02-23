0573c7b373f3f31f395412e7d2c7b6aa853e1619
minar-hossain82 <minarhossain986@gmail.com>
2026-02-23 17:28:04 +0600

    Update Working with Files/mainpromise.js

--- Working with Files/mainpromise.js
+++ Working with Files/mainpromise.js
@@ -1,10 +1,7 @@
-import fs from "fs/promises"; // Import the promise-based filesystem module
+import fs from "fs/promises";
 
-// Read the contents of the file asynchronously
 let a = await fs.readFile("harry.txt");
 
-// Append new text to the file asynchronously
 let b = await fs.appendFile("harry.txt", "\n\n\n\nthis is amazing promise");
 
-// Print the original file content and the result of appendFile (undefined)
 console.log(a.toString(), b);
