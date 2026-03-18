5e73b9a837e0e0943da3d85799b4491341a9b617
minar-hossain82 <minarhossain986@gmail.com>
2026-03-18 21:55:10 +0600

    Update javascript/Advanced Javascript/script.js

--- javascript/Advanced Javascript/script.js
+++ javascript/Advanced Javascript/script.js
@@ -1,30 +1,39 @@
+"use strict";
+
+const a1 = 6;
 console.log(a1);
 
-(async function main() {
-  let obj = {
-    a: 1,
-    b: 2,
-    c: 3,
-  };
-
-  let { a, b } = obj;
-  console.log(a, b);
-
-  let arr = [1, 4, 6];
-  console.log(await sum(arr[0], arr[1], arr[2]));
-  console.log(await sum(...arr));
-})();
-
-var a1 = 6;
-
-const sleep = async () => {
-  return new Promise((resolve) => {
+const sleep = async (ms = 1000) =>
+  new Promise((resolve) => {
     setTimeout(() => {
       resolve(45);
-    }, 1000);
+    }, ms);
   });
-};
 
-const sum = async (a, b, c) => {
-  return a + b + c;
-};
+const sum = async (a, b, c) => a + b + c;
+
+async function main() {
+  try {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    const { a, b } = obj;
+    console.log(a, b);
+
+    const arr = [1, 4, 6];
+
+    console.log(await sum(arr[0], arr[1], arr[2]));
+    console.log(await sum(...arr));
+
+    // const sleepResult = await sleep();
+    // console.log(sleepResult);
+  } catch (error) {
+    console.error("Error in main():", error);
+    process.exitCode = 1;
+  }
+}
+
+main();
