f2001a62d9c5eb81ada3ed561ad59c01a430fd90
minar-hossain82 <minarhossain986@gmail.com>
2026-04-05 20:37:58 +0600

    Update javascript/Javascript Callbacks & Promises/script.js

--- javascript/Javascript Callbacks & Promises/script.js
+++ javascript/Javascript Callbacks & Promises/script.js
@@ -23,7 +23,7 @@ const callback = (arg, fn) => {
 const loadScript = (src, callback) => {
   let sc = document.createElement("script");
   sc.src = src;
-  sc.onload = callback("Harry", fn);
+  sc.onload = () => callback("Harry", fn);
   document.head.append(sc);
 };
 
