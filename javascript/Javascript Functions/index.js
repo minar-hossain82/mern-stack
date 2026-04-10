eb8421f88c07cebbc150c12fb40b2daad60286c5
minar-hossain82 <minarhossain986@gmail.com>
2026-04-10 20:34:38 +0600

    Update javascript/Javascript Functions/index.js

--- javascript/Javascript Functions/index.js
+++ javascript/Javascript Functions/index.js
@@ -1,25 +1,25 @@
 function nice(name) {
-  console.log("Hey " + name + " you are nice!");
-  console.log("Hey " + name + " you are good!");
-  console.log("Hey " + name + " your tshirt is nice!");
-  console.log("Hey " + name + " your course is good too!");
+  console.log(`Hey ${name}, you are nice!`);
+  console.log(`Hey ${name}, you are good!`);
+  console.log(`Hey ${name}, your t-shirt is nice!`);
+  console.log(`Hey ${name}, your course is good too!`);
 }
 
 function sum(a, b, c = 3) {
-  console.log(a, b, c);
+  console.log("Inputs:", a, b, c);
   return a + b + c;
 }
 
-result1 = sum(3, 2);
-result2 = sum(7, 5);
-result3 = sum(3, 13, 1);
+const result1 = sum(3, 2);
+const result2 = sum(7, 5);
+const result3 = sum(3, 13, 1);
 
-console.log("The sum of these numbers is: ", result1);
-console.log("The sum of these numbers is: ", result2);
-console.log("The sum of these numbers is: ", result3);
+console.log("The sum of these numbers is:", result1);
+console.log("The sum of these numbers is:", result2);
+console.log("The sum of these numbers is:", result3);
 
 const func1 = (x) => {
-  console.log("I am an arrow function", x);
+  console.log("I am an arrow function:", x);
 };
 
 func1(34);
