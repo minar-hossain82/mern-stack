81e378688582503e2fd620328242850c96bf56cc
minar-hossain82 <minarhossain986@gmail.com>
2026-04-13 22:37:31 +0600

    Update javascript/Javascript variables, Data types & Objects/script.js

--- javascript/Javascript variables, Data types & Objects/script.js
+++ javascript/Javascript variables, Data types & Objects/script.js
@@ -1,42 +1,36 @@
-console.log("Hey this is tutorial 55");
+console.log("Tutorial 55: JavaScript Variables");
 
-var a = 5;
-// a = a + 1
+let a = 5;
 let b = 6;
 let c = "Harry";
 let _a = "Shubham";
-// var 55a = "Rohan"; // Not Allowed
 
-// console.log(a +  b + 8);
-// console.log(typeof a, typeof b, typeof c);
 {
-    // let a = 66;
-    console.log(a)
+  console.log(a);
 }
-console.log(a)
-// const a1 = 6;
-// a1 = a1 + 1; // Not Allowed because a1 is constant
 
+console.log(a);
 
 let x = "Harry bhai";
 let y = 22;
 let z = 3.55;
 const p = true;
-let q = undefined;
+let q;
 let r = null;
 
-console.log(x, y, z, p, q, r)
-console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
-
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
 
 let o = {
-    "name": "Harry",
-    "job code": 5600,
-    "is_handsome": true
-}
+  name: "Harry",
+  jobCode: 5600,
+  isHandsome: true,
+};
 
 console.log(o);
+
 o.salary = "100crores";
 console.log(o);
+
 o.salary = "500crores";
 console.log(o);
