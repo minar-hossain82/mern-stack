b776fa0d4af56a60b4f442ab50cbc488f25a7599
minar-hossain82 <minarhossain986@gmail.com>
2026-05-29 23:21:53 +0600

    Update javascript/Javascript Callbacks & Promises/promise.js

--- javascript/Javascript Callbacks & Promises/promise.js
+++ javascript/Javascript Callbacks & Promises/promise.js
@@ -1,31 +1,31 @@
-console.log("This is Promises");
+console.log("Promise Race Example");
 
-const prom1 = new Promise((resolve, reject) => {
-  const a = Math.random();
+const promiseOne = new Promise((resolve, reject) => {
+  const randomValue = Math.random();
 
-  if (a < 0.5) {
+  if (randomValue < 0.5) {
     reject("Random number did not support you");
   } else {
     setTimeout(() => {
-      console.log("Yes I am done");
+      console.log("Promise 1 completed");
       resolve("Harry");
     }, 3000);
   }
 });
 
-const prom2 = new Promise((resolve, reject) => {
-  const a = Math.random();
+const promiseTwo = new Promise((resolve, reject) => {
+  const randomValue = Math.random();
 
-  if (a < 0.5) {
+  if (randomValue < 0.5) {
     reject("Random number did not support you 2");
   } else {
     setTimeout(() => {
-      console.log("Yes I am done 2");
+      console.log("Promise 2 completed");
       resolve("Harry 2");
     }, 1000);
   }
 });
 
-const p3 = Promise.race([prom1, prom2]);
-
-p3.then((res) => console.log(res)).catch((err) => console.log(err));
+Promise.race([promiseOne, promiseTwo])
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
