80f78d36325db11730c82b1d4c64bb018053ad47
minar-hossain82 <minarhossain986@gmail.com>
2026-06-04 20:50:13 +0600

    Update javascript/Javascript Conditionals/index.js

--- javascript/Javascript Conditionals/index.js
+++ javascript/Javascript Conditionals/index.js
@@ -1,20 +1,20 @@
-console.log("Hello, I am conditional tutorial");
+console.log("Conditional Tutorial");
 
 const age = 1;
 
 if (age === 18) {
-  console.log("You can drive");
+  console.log("You can drive.");
 } else if (age === 0) {
   console.log("Are you kidding?");
 } else if (age === 1) {
-  console.log("Are you again kidding?");
+  console.log("Are you kidding again?");
 } else {
-  console.log("You cannot drive");
+  console.log("You cannot drive.");
 }
 
 const a = 6;
 const b = 8;
 
-const c = a > b ? a - b : b - a;
+const difference = a > b ? a - b : b - a;
 
-console.log("Difference:", c);
+console.log(`Difference: ${difference}`);
