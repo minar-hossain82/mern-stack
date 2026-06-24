27420a0fb4b3b34f09c57df9ab7843e065e4d80f
minar-hossain82 <minarhossain986@gmail.com>
2026-06-24 18:03:24 +0600

    Update javascript/Advanced Javascript/script.js

--- javascript/Advanced Javascript/script.js
+++ javascript/Advanced Javascript/script.js
@@ -1,41 +1,28 @@
 "use strict";
 
-const sleep = async (ms = 1000) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(45);
-    }, ms);
-  });
-};
-
-const sum = async (a, b, c) => {
-  return a + b + c;
-};
+const sum = async (a, b, c) => a + b + c;
 
 async function main() {
   try {
     const number = 6;
-    console.log(number);
+    console.log("Number:", number);
 
-    const obj = {
+    const user = {
       a: 1,
       b: 2,
       c: 3,
     };
 
-    const { a, b } = obj;
-    console.log(a, b);
+    const { a, b } = user;
+    console.log("Destructured values:", a, b);
 
     const numbers = [1, 4, 6];
 
-    const result1 = await sum(numbers[0], numbers[1], numbers[2]);
-    const result2 = await sum(...numbers);
+    const directSum = await sum(numbers[0], numbers[1], numbers[2]);
+    const spreadSum = await sum(...numbers);
 
-    console.log(result1);
-    console.log(result2);
-
-    // const sleepResult = await sleep();
-    // console.log(sleepResult);
+    console.log("Direct sum:", directSum);
+    console.log("Spread sum:", spreadSum);
   } catch (error) {
     console.error("Error in main():", error);
     process.exitCode = 1;
